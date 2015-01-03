ed328f80145ab4cf4a0a5bf12bdc5e0c4fea056b
ShaneIsrael <shanemisrael@gmail.com>
2015-01-03 16:10:49 -0700

    More CPU fix

--- src/main/java/com/shaneisrael/st/overlay/OverlayFrame.java
+++ src/main/java/com/shaneisrael/st/overlay/OverlayFrame.java
@@ -67,7 +67,6 @@ public class OverlayFrame extends JFrame
             public void windowClosed(WindowEvent arg0)
             {
                 isActive = false;
-                OverlayFrame.this.dispose();
             }
 
             @Override
